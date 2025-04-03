46cc7127a24dae3839598249c2c88997d9e0ba0a
techlead <adesanya1ademola@gmail.com>
2025-04-03 15:53:34 +0100

    Created application model

---
+++ server/models/application.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const applicationSchema = new mongoose.Schema({
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job',
+        required: true,
+    },
+    applicantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    message: String,
+    resumeUrl: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['applied', 'viewed', 'rejected'],
+        default: 'applied',
+    }
+}, {timestamps: true});
+
+const Application = mongoose.model('Application', applicationSchema);
+
+export default Application;
