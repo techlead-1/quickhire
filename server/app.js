2a0b398cb2dc7fbfb40ebfc8e59b403a91730c66
techlead <adesanya1ademola@gmail.com>
2025-04-03 17:34:13 +0100

    Error and other middlewares setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import {PORT, NODE_ENV} from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome')
