e15ef02a768baf8e5d014462c8bdc5e00e7222c4
techlead <adesanya1ademola@gmail.com>
2025-04-04 12:12:28 +0100

    update user profile

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,3 +11,34 @@ export const getUser = async (req, res, next) => {
     }
 }
 
+export const updateUser = async (req, res, next) => {
+    try {
+        let allowedFields = [ 'name', 'location' ]
+        if (req.user.role === 'jobseeker') {
+            allowedFields = ['name', 'location', 'bio', 'skills', 'resumeUrl'];
+        } else if (req.user.role === 'employer') {
+            allowedFields = ['name', 'location', 'companyName', 'companyWebsite', 'logoUrl', 'companyDescription'];
+        }
+
+        let userData = {}
+        for (let field of allowedFields) {
+            if (req.body[field] !== undefined) {
+                userData[field] = req.body[field];
+            }
+        }
+
+        let user = req.user;
+        Object.assign(user, userData);
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'User updated successfully.',
+            data: {
+                user: user
+            }
+        })
+    } catch (e) {
+        next(e);
+    }
+}
