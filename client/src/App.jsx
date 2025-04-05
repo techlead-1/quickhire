608b3d62c8a2943deb324c2c229a1c9ce66c452e
techlead <adesanya1ademola@gmail.com>
2025-04-05 11:55:52 +0100

    updated client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import {BrowserRouter, Router, Route, Routes} from "react-router";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </>
   )
 }
