de2f2fae700c96143edb7601b5bb04580a8852a6
techlead <adesanya1ademola@gmail.com>
2025-04-06 14:07:05 +0100

    added navbar using tailwind UI

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from "./navbar.jsx";
 
 const Index = () => {
     return (
         <div>
-            Home
+            <Navbar />
         </div>
     );
 };
