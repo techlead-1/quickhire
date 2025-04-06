54786bc5c88623e5de6a0de86c9dbd714e895059
techlead <adesanya1ademola@gmail.com>
2025-04-06 16:30:49 +0100

    component used and packages installed

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navbar from "./navbar.jsx";
+import Hero from "./hero.jsx";
+import Cta from "./cta.jsx";
+import About from "./about.jsx";
+import Footer from "@/pages/home/footer.jsx";
 
 const Index = () => {
     return (
         <div>
             <Navbar />
+            <Hero />
+            <About />
+            <Cta />
+            <Footer />
         </div>
     );
 };
