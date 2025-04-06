e37afebc5d28c1fc3bba573d5a13113951bff941
techlead <adesanya1ademola@gmail.com>
2025-04-06 16:29:54 +0100

    prebuilt navbar component

--- client/src/pages/home/navbar.jsx
+++ client/src/pages/home/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <header className={`flex w-full items-center bg-white dark:bg-dark`}>
+        <header className={`flex w-full items-center bg-white`}>
             <div className="container">
                 <div className="relative -mx-4 flex items-center justify-between">
                     <div className="w-60 max-w-full px-4">
@@ -12,12 +12,6 @@ const Navbar = () => {
                             <img
                                 src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-primary.svg"
                                 alt="logo"
-                                className="dark:hidden"
-                            />
-                            <img
-                                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-white.svg"
-                                alt="logo"
-                                className="hidden dark:block"
                             />
                         </a>
                     </div>
@@ -30,29 +24,27 @@ const Navbar = () => {
                                     open && "navbarTogglerActive"
                                 } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
                             >
-                                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
+                                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
+                                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
                             </button>
                             <nav
                                 // :className="!navbarOpen && 'hidden' "
                                 id="navbarCollapse"
-                                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${
+                                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:bg-transparent ${
                                     !open && "hidden"
                                 } `}
                             >
-                                <ul className="block lg:flex">
-                                    <ListItem NavLink="/#">Home</ListItem>
-                                    <ListItem NavLink="/#">Payment</ListItem>
-                                    <ListItem NavLink="/#">About</ListItem>
-                                    <ListItem NavLink="/#">Blog</ListItem>
+                                <ul className="block lg:hidden">
+                                    <ListItem NavLink="/#">Sign in</ListItem>
+                                    <ListItem NavLink="/#">Sign Up</ListItem>
                                 </ul>
                             </nav>
                         </div>
                         <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
                             <a
                                 href="/#"
-                                className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
+                                className="px-7 py-3 text-base font-medium text-dark hover:text-primary"
                             >
                                 Sign in
                             </a>
@@ -79,7 +71,7 @@ const ListItem = ({ children, NavLink }) => {
             <li>
                 <a
                     href={NavLink}
-                    className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
+                    className="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex"
                 >
                     {children}
                 </a>
