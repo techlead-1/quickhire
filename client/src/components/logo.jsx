c1363ed6873e19758b6d47e7b27d65e259ab73bd
techlead <adesanya1ademola@gmail.com>
2025-04-07 16:01:47 +0100

    created logo jsx

---
+++ client/src/components/logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <span className="text-3xl font-bold tracking-tight text-slate-900 font-[Outfit] fo">
+          Quick<span className="text-primary">Hire</span>
+        </span>
+    );
+};
+
+export default Logo;
