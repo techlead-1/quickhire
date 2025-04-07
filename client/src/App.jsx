74c366d145064c294b3d9588e8bd92553424f36c
techlead <adesanya1ademola@gmail.com>
2025-04-07 23:06:48 +0100

    Created working error 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from "@/pages/home/Index.jsx";
+import NotFound from "@/pages/NotFound.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
