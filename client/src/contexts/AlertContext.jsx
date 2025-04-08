2bc246ae11014d94bf95cbd721657375b16a6950
techlead <adesanya1ademola@gmail.com>
2025-04-08 17:01:01 +0100

    created alert context to make Alert method and timeout logic available by just calling a function without import

---
+++ client/src/contexts/AlertContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from 'react';
+
+const AlertContext = createContext()
+
+export const AlertProvider = ({ children }) => {
+    const [alert, setAlert] = useState(null)
+
+    const showAlert = (message, success, duration = 3000) => {
+        setAlert({message, success})
+        setTimeout(() => {setAlert(null)}, duration)
+    }
+
+    return (
+        <AlertContext.Provider value={{alert, showAlert}}>
+            {children}
+        </AlertContext.Provider>
+    )
+}
+
+export const useAlert = () => useContext(AlertContext)
