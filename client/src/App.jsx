dacebaa983e09d007b50f2ab704d57f6cedf4f02
techlead <adesanya1ademola@gmail.com>
2025-04-09 18:23:33 +0100

    added all routes to app routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,13 @@ import NotFound from "@/pages/NotFound.jsx";
 import SignIn from "@/pages/auth/SignIn.jsx";
 import SignUp from "@/pages/auth/SignUp.jsx";
 import Alert from "@/components/Alert.jsx";
-import {useState} from "react";
 import {useAlert} from "@/contexts/AlertContext.jsx";
 import JobsPage from "@/pages/jobs/JobsPage.jsx";
+import JobsForm from "@/pages/jobs/JobsForm.jsx";
+import ShowJobs from "@/pages/jobs/ShowJobs.jsx";
+import EditProfile from "@/pages/profile/EditProfile.jsx";
+import ApplicationsPage from "@/pages/applications/ApplicationsPage.jsx";
+import ShowApplications from "@/pages/applications/ShowApplications.jsx";
 
 function App() {
     const { alert } = useAlert()
@@ -20,15 +24,17 @@ function App() {
         }
 
       <Routes>
-        <Route path='/' element={
-            <Container>
-                <Home />
-            </Container>
-        } />
-        <Route path='*' element={<NotFound />} />
-        <Route path={'/auth/sign-in'} element={<SignIn />} />
-        <Route path={'/auth/sign-up'} element={<SignUp />} />
-        <Route path={'/jobs'} element={<JobsPage />} />
+          <Route path='/' element={<Container> <Home /> </Container>} />
+          <Route path={'/auth/sign-in'} element={<SignIn />} />
+          <Route path={'/auth/sign-up'} element={<SignUp />} />
+          <Route path='/profile' element={<Container> <EditProfile /> </Container>} />
+          <Route path='/jobs' element={<Container> <JobsPage /> </Container>} />
+          <Route path='/jobs/create' element={<Container> <JobsForm /> </Container>} />
+          <Route path='/jobs/edit/:id' element={<Container> <JobsForm /> </Container>} />
+          <Route path='/jobs/:id' element={<Container> <ShowJobs /> </Container>} />
+          <Route path='/applications' element={<Container> <ApplicationsPage /> </Container>} />
+          <Route path='/applications/:id' element={<Container> <ShowApplications /> </Container>} />
+          <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
