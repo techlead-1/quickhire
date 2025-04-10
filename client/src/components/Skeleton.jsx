ea41be31aa5479b8bc8bee3c99d7681b0f133d82
techlead <adesanya1ademola@gmail.com>
2025-04-10 06:33:34 +0100

    Created skeleton component

---
+++ client/src/components/Skeleton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Skeleton = () => {
+    return (
+        <section className="bg-white py-20 dark:bg-dark">
+            <div className="container">
+                <div className="mx-auto w-full max-w-[570px] space-y-4">
+                    <div className="h-3 w-full rounded-full bg-gradient-to-r from-gray-1 to-gray-4 dark:from-dark-4 dark:to-dark-5"></div>
+                    <div className="h-3 w-4/6 rounded-full bg-gradient-to-r from-gray-1 to-gray-4 dark:from-dark-4 dark:to-dark-5"></div>
+                    <div className="h-3 w-5/6 rounded-full bg-gradient-to-r from-gray-1 to-gray-4 dark:from-dark-4 dark:to-dark-5"></div>
+                    <div className="h-3 w-full rounded-full bg-gradient-to-r from-gray-1 to-gray-4 dark:from-dark-4 dark:to-dark-5"></div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Skeleton;
