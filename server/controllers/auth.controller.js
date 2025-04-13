63215bfed0efc3ebf40d4b85dcbe7f9f65700f7d
techlead <adesanya1ademola@gmail.com>
2025-04-13 14:01:40 +0100

    Fixed text issue

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -43,7 +43,7 @@ export const signUp = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: COOKIES_ENV === 'production',
-            sameSite: COOKIES_ENV === 'production' ? 'None' : 'Strict',
+            sameSite: COOKIES_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
@@ -97,7 +97,7 @@ export const signIn = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: COOKIES_ENV === 'production',
-            sameSite: COOKIES_ENV === 'production' ? 'None' : 'Strict',
+            sameSite: COOKIES_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -118,7 +118,7 @@ export const signOut = async (req, res, next) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: COOKIES_ENV === 'production',
-            sameSite: COOKIES_ENV === 'production' ? 'None' : 'Strict',
+            sameSite: COOKIES_ENV === 'production' ? 'none' : 'strict',
         });
 
         res.status(200).json({
