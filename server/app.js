90c1983e0c1fa1081534b144e303809793bbaab0
techlead-1 <adesanya1ademola@gmail.com>
2026-06-03 02:46:24 +0100

    fixed app listen

--- server/app.js
+++ server/app.js
@@ -50,11 +50,11 @@ app.use((req, res, next) => {
 
 app.use(errorMiddleware);
 
-app.listen(PORT, async (req, res) => {
-    console.log(`${NODE_ENV} server is running on port: ${PORT}`);
-    await connectToDatabase();
-
-    console.log('Connected to database');
-})
+if (NODE_ENV !== 'production') {
+    connectToDatabase();
+    app.listen(PORT, () => console.log(`Dev server on port ${PORT}`));
+} else {
+    connectToDatabase();
+}
 
 export default app;
