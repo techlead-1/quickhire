a25bd54528b186be5b0b882f61d1871a8e1ab041
techlead-1 <adesanya1ademola@gmail.com>
2026-06-03 19:41:40 +0100

    allow cross origin cookies

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -43,7 +43,7 @@ export const signUp = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: COOKIES_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
@@ -96,7 +96,7 @@ export const signIn = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: COOKIES_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -117,7 +117,7 @@ export const signOut = async (req, res, next) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: COOKIES_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
         });
 
         res.status(200).json({
